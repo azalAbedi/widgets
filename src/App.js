5db8fa8f040757141713ae6d5e8fe24739be6849
Azal Abedi <azal.abedi@gmail.com>
2021-06-21 21:37:57 -0600

    set up Search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+// import Accordion from './components/Accordion';
+import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front-end JS library for building user interfaces.'
-  },
-  {
-    title: 'Why use React?',
-    content: 'According to Stack Overflow survey 2020, it is the most popular JS library.'
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components.'
-  }
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front-end JS library for building user interfaces.'
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'According to Stack Overflow survey 2020, it is the most popular JS library.'
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components.'
+//   }
+// ];
 
 function App() {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 }

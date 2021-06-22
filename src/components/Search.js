e875614e85c83ba5d472d996f871364b7ecf5003
Azal Abedi <azal.abedi@gmail.com>
2021-06-21 22:06:19 -0600

    render list of searches from Wiki API

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Search() {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("film");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
@@ -15,10 +16,23 @@ function Search() {
           srsearch: term,
         },
       });
+
+      setResults(data.query.search);
     };
 
-    search();
+    search(); // if you want to, put this in an IF (term) { search(); } block
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -32,6 +46,7 @@ function Search() {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 }

b256f6f3858e54e59397380584574a497f525755
Azal Abedi <azal.abedi@gmail.com>
2021-06-21 18:15:54 -0600

    build components/Accordion, initial set up with semantic UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front-end JS library for building user interfaces.'
+  },
+  {
+    title: 'Why use React?',
+    content: 'According to Stack Overflow survey 2020, it is the most popular JS library.'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components.'
+  }
+];
 
 function App() {
   return (
-    <h1>Widgets App</h1>
+    <div>
+      <Accordion items={items} />
+    </div>
   );
 }
 

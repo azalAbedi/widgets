d8955be0e3cc6aa3b7ad6677d4031096bf4947a1
Azal Abedi <azal.abedi@gmail.com>
2021-06-22 20:10:32 -0600

    set up a Google Translate API call via Axios

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,11 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 function Convert({ language, text }) {
   useEffect(() => {
-    
-  }, [language, text])
-  
+    axios.post(
+      "https://translation.googleapis.com/language/translate/v2",
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: `${process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY}`,
+        },
+      }
+    );
+  }, [language, text]);
+
   return <div />;
-};
+}
 
 export default Convert;

3d5f80eae08c1f25f048087a5764994b3dd54fc8
Azal Abedi <azal.abedi@gmail.com>
2021-06-23 16:09:37 -0600

    handle Ctrl or Cmd Click events

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,10 @@ import React from "react";
 
 function Link({ className, href, children }) {
   const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault();
     window.history.pushState({}, '', href);
 

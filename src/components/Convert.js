83d2cc8c7d941fd956456409467a064353e565c1
Azal Abedi <azal.abedi@gmail.com>
2021-06-22 20:02:57 -0600

    set up Convert component

---
+++ src/components/Convert.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function Convert({ language, text }) {
+  useEffect(() => {
+    
+  }, [language, text])
+  
+  return <div />;
+};
+
+export default Convert;

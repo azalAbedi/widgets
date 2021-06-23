f74a730ba28a633d01bf71b55b92f5d39cd143a0
Azal Abedi <azal.abedi@gmail.com>
2021-06-22 20:17:02 -0600

    implement and display translation on screen

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,21 +4,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 function Convert({ language, text }) {
+  const [translated, setTranslated] = useState("");
+
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: `${process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY}`,
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: `${process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY}`,
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
   }, [language, text]);
 
-  return <div />;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 }
 
 export default Convert;
